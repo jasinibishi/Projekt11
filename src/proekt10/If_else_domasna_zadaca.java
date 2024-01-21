9ac0039c3b06b42b5f3e5354ddad317d8f095b04
Jasin Ibishi <jasinibishi@gmail.com>
2024-01-21 14:58:09 +0100

    first commit

---
+++ src/proekt10/If_else_domasna_zadaca.java
@@ -0,0 +1,27 @@
+package proekt10;
+
+public class If_else_domasna_zadaca {
+
+	public static void main(String[] args) {
+		int age = 25;
+		if(age > 0)
+		{
+			if( age > 16 )
+			{
+				System.out.println("Welcome!");
+			}
+			else
+			{
+				System.out.println("Too Young !");
+			}
+			
+		}
+		else 
+		{
+			System.out.println("Error");
+		}
+		// Outputs "Welcome!"
+
+	}
+
+}
